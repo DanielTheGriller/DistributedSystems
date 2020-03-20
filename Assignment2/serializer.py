b3e21a6818a37c926b6b5aa8098bd2b4a79c2dac
DanielTheGriller <31897321+DanielTheGriller@users.noreply.github.com>
2020-03-20 20:12:01 +0200

    Update serializer.py

--- Assignment2/serializer.py
+++ Assignment2/serializer.py
@@ -9,7 +9,7 @@ import msgpack
 import yaml
 from timeit import default_timer as timer
 import matplotlib
-matplotlib.use('SVG')
+matplotlib.use('PyQt5')
 import matplotlib.pyplot as plt 
 """ This program serializes and deserializes data
 using different formats. It compares the processing times
